2e9abd4ca9e5484964d41ffa0431e694984d2e1a
LeeKangJin <himnae5181@naver.com>
2019-04-06 12:22:47 +0900

    자주쓰는 코드

---
+++ Comparator.java
@@ -0,0 +1,34 @@
+
+// struct -> int x, int y 
+// x순 내림차순 정렬, x가 같을 때 y순 내림차순 정렬 Comparator
+
+
+class order implement Comparator<struct>{
+
+  public int compare(struct a, struct b){
+  
+    int firstXValue = a.x;
+    int secodXvalue = b.x;
+    
+    int firstYValue = a.y;
+    int secondYValue = b.y;
+    
+    if(firstXValue < secondXValue){
+      return -1;
+    }
+    else if(firstXValue > secondXValue){
+      return 1;
+    }
+    else{
+    
+      if(firstYValue < secondYValue) return -1;
+      else if(firstYValue > secondYValue ) return 1;
+      else return 0;
+    
+    
+    }
+  
+  }
+
+
+}
