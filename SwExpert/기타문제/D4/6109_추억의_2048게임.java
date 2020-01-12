50015841472125a9cd0a19549fcd4e173b85b3c7
LeeKangJin <himnae5181@naver.com>
2020-01-13 08:53:01 +0900

    Update 6109_추억의_2048게임.java

--- SwExpert/기타문제/D4/6109_추억의_2048게임.java
+++ SwExpert/기타문제/D4/6109_추억의_2048게임.java
@@ -1,33 +1,25 @@
-import java.util.*;
-import java.io.*;
- 
-class Solution{
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        Scanner in=new Scanner(System.in);
-        int T;
-        T=in.nextInt();
-        int Max=100001,max=-1,max_id=0;
-        int cnt[] = new int[Max];
-         
-        for(int i=1;i<Max;i++){
-            for(int j=i;j<Max;j+=i){
-                cnt[j]++;   
-            }
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution {   
+   public static void main(String[] args) {
+      // TODO Auto-generated method stub
+      Scanner sc = new Scanner(System.in);
+      int i=1;
+      ArrayList<Long> sosu = new ArrayList<>();
+      sosu.add((long)1);
+      sosu.add((long)2);
+      while(sosu.size()<92) {
+         sosu.add(sosu.get(i)+sosu.get(i-1));
+            i++;
         }
-        for(int i=1;i<Max;i++){
-            if(cnt[i]>=max){
-                max=cnt[i];
-                max_id=i;
-            }
-            cnt[i]=max_id;
-        }
-         
-         
-        for(int test_case=1; test_case<=T;test_case++){
-           int num;
-            num=in.nextInt();
-            System.out.println("#"+ test_case+" "+cnt[num]);
-        }
-         
-    }
+      int allCase = sc.nextInt();
+      int val;
+      for(int testCase = 1;testCase<=allCase;testCase++) {
+         val = sc.nextInt();
+         System.out.format("#%d %d %d\n",testCase,sosu.get(val),sosu.get(val-1));
+      }
+      
+   }
+   
 }
