23399a187f27fd75e11abd2f454ccbf4618f2149
LeeKangJin <himnae5181@naver.com>
2020-01-13 08:45:56 +0900

    Update 4530_극학의_청소_작업.java

--- SwExpert/기타문제/D4/4530_극학의_청소_작업.java
+++ SwExpert/기타문제/D4/4530_극학의_청소_작업.java
@@ -1,73 +1,33 @@
-import java.util.Scanner;
-
-class Solution
-{
-   public static void main(String[] args) {
-      // TODO Auto-generated method stub
-      Scanner sc = new Scanner(System.in);
-      int T;
-      T=sc.nextInt();
-      long start;
-      long end;
-      String a;
-      count_floor startFloor;
-      count_floor endFloor;
-      for(int test_case = 1; test_case <= T; test_case++)
-      {
-         start = sc.nextLong();
-         end = sc.nextLong();
-         startFloor = new count_floor(start);
-         endFloor = new count_floor(end);
-         System.out.print("#");
-         System.out.print(test_case);
-         System.out.print(" ");
-         if(endFloor.returnGround() == startFloor.returnGround()) 
-            System.out.println(endFloor.calFloor()-startFloor.calFloor());
+import java.util.*;
+import java.io.*;
+ 
+class Solution{
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        Scanner in=new Scanner(System.in);
+        int T;
+        T=in.nextInt();
+        int Max=100001,max=-1,max_id=0;
+        int cnt[] = new int[Max];
          
-         else
-            System.out.println(endFloor.calFloor()-startFloor.calFloor()-1);
+        for(int i=1;i<Max;i++){
+            for(int j=i;j<Max;j+=i){
+                cnt[j]++;   
+            }
+        }
+        for(int i=1;i<Max;i++){
+            if(cnt[i]>=max){
+                max=cnt[i];
+                max_id=i;
+            }
+            cnt[i]=max_id;
+        }
          
-      }
-   }
-   
-}
-class count_floor{
-   long floor;
-   boolean ground;
-   int numberOfDigits;
-   count_floor(long floor){
-      if(floor>0) {
-         this.floor = floor;
-         this.ground = true;
-      }
-      else {
-         this.floor = -floor;
-         this.ground = false;
-      }
-      
-      String str = String.valueOf(this.floor);
-      
-      numberOfDigits = str.length();
-   }
-   boolean returnGround() {
-      return ground;
-   }
-   
-   long calFloor() {
-      long val = 0;
-      long i=1;
-      long thisFloor = floor;
-      long order;
-      while(true) {
          
-         order = thisFloor%10;
-         if(order>3) order = order-1;
-         val = val + order * i;
-         i = i * 9;
-         if(thisFloor<10)break;
-         else thisFloor = thisFloor/10;
-      }
-      if(ground) return val;
-      else return -val;
-   }
+        for(int test_case=1; test_case<=T;test_case++){
+           int num;
+            num=in.nextInt();
+            System.out.println("#"+ test_case+" "+cnt[num]);
+        }
+         
+    }
 }
